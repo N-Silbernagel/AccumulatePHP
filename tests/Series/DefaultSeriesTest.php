15fde084db0a51d93cfb604e7a3e3c7fd7a4276b
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2022-09-25 21:05:12 +0200

    fix generic in DefaultSeriesTest

--- tests/Series/DefaultSeriesTest.php
+++ tests/Series/DefaultSeriesTest.php
@@ -151,7 +151,7 @@ final class DefaultSeriesTest extends TestCase
     /** @test */
     public function it_should_keep_track_of_its_current_key()
     {
-        /** @var DefaultSeries<int, int> $defaultSeries */
+        /** @var DefaultSeries<int> $defaultSeries */
         $defaultSeries = DefaultSeries::fromArray([0, 1, 2]);
 
         self::assertSame(0, $defaultSeries->key());
